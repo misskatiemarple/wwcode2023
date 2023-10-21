7d19f55f3c4da2cf66719c6e4cc54f32548f485a
misskatiemarple <misskatiemarple@gmail.com>
2023-10-21 21:55:08 +0900

    Landing page and hero updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import SearchEngineSkills from "./SearchEngineSkills.js";
 import SearchNoSkills from "./SearchNoSkills";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-
 import "./App.css";
 
 function App() {
@@ -28,17 +27,17 @@ function App() {
             <ul class="navbar-nav">
               <li class="nav-item active">
                 <a class="nav-link" href="#">
-                  Home <span class="sr-only">(current)</span>
+                  Home
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Skills Taxonomy
+                  Blah
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Resources
+                  Up-skill & Re-skill
                 </a>
               </li>
             </ul>
@@ -46,55 +45,81 @@ function App() {
         </nav>
       </div>
       <div className="App">
-        <div className="hero">
-          <h1 className="hero-h1">Welcome Back To The Workforce, Mama</h1>
-          <h2 className="hero-h2">Let's find your next dream job!</h2>
+        <div className="hero-container">
+          <div
+            className="hero-image"
+            alt="a woman's hands are resting on a laptop keyboard"
+          >
+            <h3 className="hero-h3">..HEY MAMA..</h3>
+            <h1 className="hero-h1">Want to rejoin the workforce?</h1>
+            <h2 className="hero-h4">Find your next dream job</h2>
+          </div>
         </div>
-        <body>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <SearchEngineSkills />
-          <SearchNoSkills />
-        </body>
+        <div className="page-container">
+          <body>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <SearchEngineSkills />
+            <SearchNoSkills />
+          </body>
+        </div>
       </div>
       <div class="footer">
         <div class="address-box">
           <ul>
-            <li>Address Line 1</li>
-            <li>Address Line 2</li>
-            <li>Address 3</li>
+            <li>A Better World</li>
+            <li>Tokyo</li>
+            <li>Japan</li>
+            <li>〒279-0031</li>
           </ul>
         </div>
         <div class="code-msg-box">
           <p>
-            Coded by:{" "}
+            Coded by{" "}
             <a
               href="https://github.com/misskatiemarple/wwcode2023"
               target="_blank"
               rel="nonreferrer"
+              className="link-override"
             >
-              Katie
-            </a>{" "}
-            and
+              Katie{" "}
+            </a>
+            and{" "}
             <a
               href="https://github.com/coffeeB4Ugo"
               target="_blank"
               rel="nonreferrer"
+              className="link-override"
             >
               Kalie
             </a>
           </p>
+          <p className="mb-0">
+            <strong>
+              Submission project for the{" "}
+              <a
+                href="https://www.womenwhocode.com/network/tokyo/"
+                target="_blank"
+                rel="nonrefferer"
+                className="link-override"
+              >
+                Women Who Code Tokyo
+              </a>{" "}
+            </strong>
+          </p>
+          <p className="mt-0">
+            <strong>Hackathon for Social Good 2023</strong>
+          </p>
         </div>
         <div class="brand-stamp-box">
-          <p>WWCode</p>
-          <p>Tokyo</p>
+          <img src="/public/WWCodelogo.jpeg" alt="Women Who Code Logo" />
         </div>
       </div>
     </div>
