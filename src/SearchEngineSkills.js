487ceddcfaee9a914c764597abc8756c0104b905
misskatiemarple <misskatiemarple@gmail.com>
2023-10-22 11:52:35 +0900

    Search engine updated

--- src/SearchEngineSkills.js
+++ src/SearchEngineSkills.js
@@ -1,20 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function SearchEngineSkills() {
-  let [skill, setSkill] = useState("");
+  const [skill, setSkill] = useState("");
+  const [job, setJob] = useState();
 
+  function handleResponse(response) {
+    setJob(response.data.employerId);
+    console.log(response.data);
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`searching for ${skill}`);
+    searchAPI();
   }
+
+  useEffect(() => {
+    searchAPI();
+  }, []);
+
+  function searchAPI() {
+    const apiKey = " 0d349a16-226b-4dbb-bce0-05eba19e3183";
+    const apiUrl = " https://www.reed.co.uk/api/1.0/search?keywords={skill}";
+    /*Have deleted location due to lack of Japan jobs on this free API. We'll need to address this in the future if we choose to continue the project*/
+
+    axios
+      .get(apiUrl, {
+        headers: {
+          Authorization: "Basic " + btoa(apiKey + ":"),
+        },
+      })
+      .then(handleResponse)
+      .catch((error) => console.error(error));
+  }
+  /*why are we getting a CORS error here?*/
+
   function updateSkill(event) {
+    event.preventDefault();
     setSkill(event.target.value);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="search"
+        type="text"
         placeholder="Previous work, skills..."
         autoFocus={true}
         onChange={updateSkill}
